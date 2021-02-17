59903d7dfeba777e32749ba56c030f4470651228
Andrew Aramouni <aramouni8@gmail.com>
2021-02-16 19:26:14 -0500

    added some ES6 notes...NOOIICCE JOB!

---
+++ ES6_notes.js
@@ -0,0 +1,50 @@
+var param = 'size'
+var config = {
+    [param]: 12,
+    ['mobile' + param.charAt(0).toUpperCase()
+        + param.slice(1)]: 4//param.slice(1) just means the rest of the array item will show. This case it is the second character of the item and every character after.
+    //example, param.slice(1) (the item in the array is 'size')just adds 'ize' because slice just shows the number in the character and show the character and all the characters after that in that item(size)ok.....i get it lol.
+}
+console.log(config.mobileSize)
+
+// Object.assign() in ES6
+// ES6 adds a new Object method assign() that allows us to combine multiple sources into one target to create a single new object.
+// Object.assign() is also useful for creating a duplicate of an existing object.
+
+// Let's look at the following example to see how to combine objects:
+let person = {
+    name: 'Jack',
+    age: 18,
+    sex: 'male'
+}
+let student = {
+    name: 'Bob',
+    age: 20,
+    xp: '2'
+}
+let newStudent = Object.assign({}, person, student)
+console.log(newStudent)
+// Here/\/\/\/\ we used Object.assign() where the first parameter is the target object you want to apply new properties to.
+// Every parameter after the first will be used as sources for the target. There are no limitations on the number of source parameters. However, order is important because properties in the second parameter will be overridden by properties of the same name in third parameter, and so on.
+
+// In the example above, we used a new object {} as the target and used two objects as sources.
+// Try changing the order of second and third parameters to see what happens to the result.
+// To avoid this (mutations), use Object.assign() to create a new object.
+// For example:
+let person = {
+    name: 'Jack',
+    age: 18
+}
+let newPerson = Object.assign({}, person)
+newPerson.name = 'Bob'
+console.log(person.name) // Jack
+console.log(newPerson.name) //Bob
+
+// Finally, you can assign a value to an object property in the Object.assign() statement.
+// For example:
+let person = {
+    name: 'Jack',
+    age: 18
+}
+let newPerson = Object.assign({}, person, { name: 'Bob' })
+
--- index.js
+++ index.js
@@ -1,36 +1,47 @@
 var images = [
-    "http://www.sololearn.com/uploads/slider/1.jpg", 
-    "http://www.sololearn.com/uploads/slider/2.jpg", 
+    "http://www.sololearn.com/uploads/slider/1.jpg",
+    "http://www.sololearn.com/uploads/slider/2.jpg",
     "http://www.sololearn.com/uploads/slider/3.jpg"
- ];
- var prevImg = document.getElementById('prevImg')
- var nextImg = document.getElementById('nextImg')
- var prevOrNextImg = document.getElementById('slider')
- var prevSlider = 2;
- var nextSlider = 1;
- //event for previous Image button
- prevImg.addEventListener("click", function(){
-       if(images[prevSlider] >= images[0]){
-        prevOrNextImg.src = images[prevSlider];
-         prevSlider -= 1
-         console.log(prevSlider)
-       }
-       if(prevSlider === -1){
-          prevSlider = 2
-       }
- })
- //event for the next image button
- nextImg.addEventListener("click", function(){
-     if(images[nextSlider] <= images[images.length - 1]){
-        prevOrNextImg.src = images[nextSlider]
-        nextSlider += 1
-        console.log(nextSlider)
-     }
-     if(nextSlider === 3){
-         nextSlider = 0
-     }
+];
+var prevImg = document.getElementById('prevImg')
+var nextImg = document.getElementById('nextImg')
+var prevOrNextImg = document.getElementById('slider')
+var prevSlider = 2;
+var nextSlider = 1;
+var num = 0;
+//event for previous Image button
+prevImg.addEventListener("click", function () {
+    num -= 1
+    console.log(num)
+    if (num < 0 || 2 === num) {
+        num = 2
+    }
+    prevOrNextImg.src = images[num];
+})
+//event for the next image button
+nextImg.addEventListener("click", function () {
+    num += 1
+    console.log(num)
+    if (num > 2 || num === 0) {
+        num = 0
+    }
+    prevOrNextImg.src = images[num]
 
- })
+})
+
+function validate() {
+    var n1 = document.getElementById('num1')
+    var n2 = document.getElementById('num2')
+    if (n1.value != "" && n2.value != "") {
+        if (n1.value == n2.value) {
+            return alert("Your in!");
+        }
+    }
+    //The form will not get submitted if its onsubmit event returns false.
+    alert("The values should be equal and not blank")
+    return false
+}
+
  //next time, figure out a way to compare the same number in both events...
  //example, i click on prevSlide and its at 2, compare a number if I want to 
  // click on next after prev btn. 
